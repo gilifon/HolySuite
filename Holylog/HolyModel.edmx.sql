1af720f5f2c73fcf6075d1effb26c20766c6ef1f
Gil <gilifon@gmail.com>
2015-05-06 20:38:57 +0300

    HolySuite First Commit

---
+++ Holylog/HolyModel.edmx.sql
@@ -0,0 +1,52 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 05/03/2015 08:41:46
+-- Generated from EDMX file: C:\Users\gill\Documents\Visual Studio 2013\Projects\IARC\Holylog\HolyModel.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [Database1];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Entity1Set'
+CREATE TABLE [dbo].[Entity1Set] (
+    [Id] int IDENTITY(1,1) NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Entity1Set'
+ALTER TABLE [dbo].[Entity1Set]
+ADD CONSTRAINT [PK_Entity1Set]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
