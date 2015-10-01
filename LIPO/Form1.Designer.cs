fb64bb7b0d801147a44070f3c8976362b2540feb
Gil <gilifon@gmail.com>
2015-10-01 13:22:25 +0300

    Temporary Adding LIPO - Live IP Observer to this solution

---
+++ LIPO/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace LIPO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.main_title = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(38, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 153);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(329, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 153);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(620, 83);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(228, 153);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(38, 270);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(228, 153);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(329, 270);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(228, 153);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(620, 270);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(228, 153);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // main_title
+            // 
+            this.main_title.AutoSize = true;
+            this.main_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.main_title.Location = new System.Drawing.Point(275, 15);
+            this.main_title.Name = "main_title";
+            this.main_title.Size = new System.Drawing.Size(343, 55);
+            this.main_title.TabIndex = 6;
+            this.main_title.Text = "Live IP Monitor";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 441);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(891, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 463);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.main_title);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label main_title;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ LIPO/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace LIPO
+{
+    
+    public partial class Form1 : Form
+    {
+        private static System.Timers.Timer aTimer;
+        List<Station> stations;
+        BackgroundWorker bw;
+
+        public Form1()
+        {
+            InitializeComponent();
+            stations = new List<Station>(6);            
+
+            var xml = XDocument.Load(@"stations.xml");
+            var query = from c in xml.Root.Descendants("station")
+                        where (int)c.Attribute("id") < 7
+                        select new Station
+                        {
+                            stationName = c.Element("name").Value,
+                            stationIP = c.Element("ip").Value
+                        };            
+            stations.InsertRange(0,query);
+
+            if (stations.Count < 6)
+            {
+                for (int i=6-stations.Count; i > 0; i--)
+                {
+                    stations.Add(new Station());
+                }
+            }
+            
+            button1.Text = stations[0].stationName;
+            button2.Text = stations[1].stationName;
+            button3.Text = stations[2].stationName;
+            button4.Text = stations[3].stationName;
+            button5.Text = stations[4].stationName;
+            button6.Text = stations[5].stationName;
+            
+            
+            bw = new BackgroundWorker();
+            bw.DoWork += bw_DoWork;
+            bw.RunWorkerCompleted += bw_RunWorkerCompleted;
+            bw.RunWorkerAsync();
+        }
+
+        void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Busy...";
+            bw.RunWorkerAsync();
+        }
+
+        void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Pinger(button1, stations[0].stationIP);
+            Pinger(button2, stations[1].stationIP);
+            Pinger(button3, stations[2].stationIP);
+            Pinger(button4, stations[3].stationIP);
+            Pinger(button5, stations[4].stationIP);
+            Pinger(button6, stations[5].stationIP);
+            toolStripStatusLabel1.Text = "Done";
+            System.Threading.Thread.Sleep(2000);
+        }
+
+        public static bool Ping(string ip)
+        {
+            Ping pingSender = new Ping();
+            IPAddress address;
+            if (IPAddress.TryParse(ip, out address))
+            {
+                PingReply reply = pingSender.Send(address);
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private void Pinger(Button sender, string ip)
+        {
+            if (Ping(ip))
+            {
+                sender.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                sender.BackColor = Color.Red;
+            }
+        }
+
+        private void setStation(int index)
+        {
+            Settings settings = new Settings(stations[index].stationName, stations[index].stationIP);
+            settings.ShowDialog();
+            if (settings.DialogResult == System.Windows.Forms.DialogResult.OK)
+            {
+                stations[index].stationName = settings.StationName;
+                stations[index].stationIP = settings.StationIP;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            setStation(0);
+            ((Button)sender).Text = stations[0].stationName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            setStation(1);
+            ((Button)sender).Text = stations[1].stationName;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            setStation(2);
+            ((Button)sender).Text = stations[2].stationName;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            setStation(3);
+            ((Button)sender).Text = stations[3].stationName;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            setStation(4);
+            ((Button)sender).Text = stations[4].stationName;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            setStation(5);
+            ((Button)sender).Text = stations[5].stationName;
+        }
+        
+
+        private void propertiesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+    public class Station
+    {
+        public string stationName { get; set; }
+        public string stationIP { get; set; }
+
+        public Station()
+        {
+            stationName = "";
+            stationIP = "";
+        }
+
+        public Station(string Name, string IP)
+        {
+            stationName = Name;
+            stationIP = IP;
+        }
+    }
+}
