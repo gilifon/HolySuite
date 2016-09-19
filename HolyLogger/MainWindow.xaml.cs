7e6af6d3f46821be2f0f4b8643223c910a4689d5
gilifon <gilifon@gmail.com>
2016-09-19 21:53:55 +0300

    HolyLogger 1st Checkin

---
+++ HolyLogger/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HolyLogger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Lock_Btn_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            TB_MyCallsign.IsEnabled = !(TB_MyCallsign.IsEnabled);
+            TB_MyGrid.IsEnabled = !(TB_MyGrid.IsEnabled);
+            if (TB_MyGrid.IsEnabled) ((Image)sender).Opacity = 1;
+            else ((Image)sender).Opacity = 0.5;
+        }
+
+        private void RefreshDateTime_Btn_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            DatePicker_QsoDate.SelectedDate = DateTime.Now.Date;
+            TB_QsoTime.Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
