2d2ca8132917068f16bbb1d0ee35a7e12acf89b3
Gil (4Z1KD) <gil4z1kd@gmail.com>
2018-05-14 23:47:27 +0300

    different map source

--- ClusterMap/MainWindow.xaml.cs
+++ ClusterMap/MainWindow.xaml.cs
@@ -1,11 +1,16 @@
 ﻿using HolyParser;
+using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
 using System.Device.Location;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media.Imaging;
 using WebSupergoo.ABCpdf11;
@@ -18,92 +23,93 @@ namespace ClusterMap
     public partial class MainWindow : Window
     {
         public const bool IS_DEBUG = false;
-        public const double PIXLES_PER_DISTANCE = 0.033;// RADIUS / 15000f;
+        public const double PIXLES_PER_DISTANCE = 0.027;// RADIUS / 15000f;
         public double lat { get; set; }
         public double lng { get; set; }
+        private static readonly HttpClient client = new HttpClient();
+
+        string FILE_PATH_PDF = Path.GetTempPath() + "map.pdf";
+        string FILE_PATH_TIFF = Path.GetTempPath() + "map.tif";
+        string FILE_PATH_PNG = Path.GetTempPath() + "temp_map.png";
+        string FILE_PATH_PNG_FINAL = Path.GetTempPath() + "map.png";
+        string MAP_URL;
 
         public MainWindow()
         {
             InitializeComponent();
             //HamQTH country = Services.getHamQth("PP5DZ");
-            lat = 32.270068;
-            lng = 35.080606;
+            //lat = 32.270068;
+            //lng = 35.080606;
+            lat = 0;
+            lng = 0;
+
             LoadDefaultMap();
         }
 
-        private void LoadDefaultMap()
+        private async void LoadDefaultMap()
         {
-            string FILE_PATH_PDF = Path.GetTempPath() + "map.pdf";
-            string FILE_PATH_TIFF = Path.GetTempPath() + "map.tif";
-            string FILE_PATH_PNG = Path.GetTempPath() + "map.png";
-            string DOWNLOADER_URI = "https://ns6t.net/azimuth/code/azimuth.fcgi?title=&location=" + lat + "%2C" + lng + "&distance=15000&paper=LETTER&bluefill=on&view=on&submit=&iplocationused=false";
-            //const string DOWNLOADER_URI = "https://ns6t.net/azimuth/code/azimuth.fcgi?title=&location=0%2C+0&distance=15000&paper=LETTER&bluefill=on&view=on&submit=&iplocationused=false";
-            if (IS_DEBUG || !File.Exists(FILE_PATH_PNG))
+            if (IS_DEBUG || !File.Exists(FILE_PATH_PNG_FINAL))
             {
-                using (var writeStream = File.OpenWrite(FILE_PATH_PDF))
-                {
-                    var httpRequest = WebRequest.Create(DOWNLOADER_URI) as HttpWebRequest;
-                    var httpResponse = httpRequest.GetResponse();
-                    httpResponse.GetResponseStream().CopyTo(writeStream);
-                    writeStream.Close();
-                    Doc theDoc = new Doc();
-                    theDoc.Read(FILE_PATH_PDF);
-                    // set up the rendering parameters
-                    theDoc.Rendering.ColorSpace = XRendering.ColorSpaceType.Rgb;
-                    theDoc.Rendering.BitsPerChannel = 8;
-                    theDoc.Rendering.DotsPerInchX = 96;
-                    theDoc.Rendering.DotsPerInchY = 96;
-                    // loop through the pages
-                    int n = theDoc.PageCount;
-                    for (int i = 1; i <= n; i++)
-                    {
-                        theDoc.PageNumber = i;
-                        theDoc.Rect.String = theDoc.CropBox.String;
-                        theDoc.Rendering.SaveAppend = (i != 1);
-                        //theDoc.Rendering.SaveCompression = XRendering.Compression.G4;
-                        theDoc.SetInfo(0, "ImageCompression", "4");
-                        theDoc.Rendering.Save(FILE_PATH_TIFF);
-                    }
-                    theDoc.Clear();
-
-                    MapPanel.Stretch = System.Windows.Media.Stretch.Uniform;
-
-                    //Rectangle cropRect = new Rectangle(0, 145, 815, 760);
-                    Rectangle cropRect = new Rectangle(30, 150, 755, 755);
-                    Bitmap src = Image.FromFile(FILE_PATH_TIFF) as Bitmap;
-                    Bitmap target = new Bitmap(cropRect.Width, cropRect.Height);
-
-                    using (Graphics g = Graphics.FromImage(target))
-                    {
-                        g.DrawImage(src, new Rectangle(0, 0, target.Width, target.Height),
-                                         cropRect,
-                                         GraphicsUnit.Pixel);
-                        target.Save(FILE_PATH_PNG, ImageFormat.Png);
-                    }
-                }
+                string response = await PostMapRequest();
+                SaveMap(response);
             }
-            MapPanel.Source = new BitmapImage(new System.Uri(FILE_PATH_PNG));
 
-            double destination_Lat = 19.929857;
-            double destination_Lng = -155.738912;
+            MapPanel.Source = new BitmapImage(new System.Uri(FILE_PATH_PNG_FINAL));
 
-            double distance = Utils.DistanceBetweenPlaces(lat, lng, destination_Lat, destination_Lng);
-            Tuple<double,double> bearing = Utils.BearingVectors(lat, lng, destination_Lat, destination_Lng);
-            double lat_vect = distance * bearing.Item1;
-            double lng_vect = distance * bearing.Item2;
-            PaintPoint(lat_vect, lng_vect);
+            ////Tanzania
+            double Tanzania_Lat = -42.233530;
+            double Tanzania_Lng = 146.168648;
+
+            ////South Argentina
+            double SA_Lat = -54.601227;
+            double SA_Lng = -71.524232;
+
+            ////BearingSea
+            double BS_Lat = 65.891002;
+            double BS_Lng = -168.940600;
+
+            ////BearingSea
+            double India_Lat = 8.300320;
+            double India_Lng = 77.472064;
+
+            ////BearingSea
+            double NP_Lat = 90;
+            double NP_Lng = 0;
+
+            ////BearingSea
+            double SP_Lat = -90;
+            double SP_Lng = 0;
+
+            ////BearingSea
+            double Liberia_Lat = 4.451074;
+            double Liberia_Lng = -7.600905;
+
+
+
+            PaintRelativePoint(lat,lng,Tanzania_Lat,Tanzania_Lng);
+            PaintRelativePoint(lat, lng, SA_Lat, SA_Lng);
+            PaintRelativePoint(lat, lng, BS_Lat, BS_Lng);
+            PaintRelativePoint(lat, lng, India_Lat, India_Lng);
+            PaintRelativePoint(lat, lng, NP_Lat, NP_Lng);
+            PaintRelativePoint(lat, lng, SP_Lat, SP_Lng);
+            PaintRelativePoint(lat, lng, Liberia_Lat, Liberia_Lng);
         }
 
-        private void PaintPoint(double lat, double lng)
+        private void PaintRelativePoint(double slat, double slng, double dlat, double dlng)
         {
+            double distance = Utils.DistanceBetweenPlaces(slat, slng, dlat, dlng);
+            Tuple<double, double> bearing = Utils.BearingVectors(slat, slng, dlat, dlng);
+            double lat_vect = distance * bearing.Item1;
+            double lng_vect = distance * bearing.Item2;
+
             System.Windows.Shapes.Ellipse point = new System.Windows.Shapes.Ellipse();
             point.Stroke = System.Windows.Media.Brushes.Red;
             point.Fill = System.Windows.Media.Brushes.Red;
             point.StrokeThickness = 1;
             point.Width = 4;
             point.Height = 4;
-            System.Windows.Controls.Canvas.SetLeft(point, (MapPanel.Source.Width / 2) + lng2pix(lng) - point.Width / 2);
-            System.Windows.Controls.Canvas.SetTop(point, (MapPanel.Source.Height / 2) - lat2pix(lat) - point.Height / 2);
+            System.Windows.Controls.Canvas.SetLeft(point, (MapPanel.Source.Width / 2) + lng2pix(lng_vect) - point.Width / 2);
+            System.Windows.Controls.Canvas.SetTop(point, (MapPanel.Source.Height / 2) - lat2pix(lat_vect) - point.Height / 2);
 
             MapContainer.Children.Add(point);
         }
@@ -116,6 +122,50 @@ namespace ClusterMap
         {
             return value * PIXLES_PER_DISTANCE;
         }
+
+        private async Task<string> PostMapRequest()
+        {
+            const string DOWNLOADER_URI = "http://ok2pbq.atesystem.cz/prog/qso2.php";
+            var values = new Dictionary<string, string>
+            {
+               { "MapSize", "900" },
+               { "range", "17000" },
+               { "QRA", "4Z1KD" },
+               { "QTH", "JJ00AA" },
+               { "tool", "E" }
+            };
+            var content = new FormUrlEncodedContent(values);
+            var response = await client.PostAsync(DOWNLOADER_URI, content);
+            var responseString = await response.Content.ReadAsStringAsync();
+            return responseString;
+        }
+
+        private void SaveMap(string response)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(response);
+            HtmlNode node = doc.DocumentNode.SelectSingleNode("//html/body/img");
+            var value = node.Attributes["src"].Value;
+            string fileName = value.Split(new char[] { '/' }).Last();
+            string url = "http://ok2pbq.atesystem.cz/image/" + fileName;
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(new Uri(url), FILE_PATH_PNG_FINAL);
+            }
+
+            //Rectangle cropRect = new Rectangle(200, 200, 900, 900);
+            //Bitmap src = Image.FromFile(FILE_PATH_PNG) as Bitmap;
+            //Bitmap target = new Bitmap(cropRect.Width, cropRect.Height);
+
+            //using (Graphics g = Graphics.FromImage(target))
+            //{
+            //    g.DrawImage(src, new Rectangle(0, 0, target.Width, target.Height),
+            //                     cropRect,
+            //                     GraphicsUnit.Pixel);
+            //    target.Save(FILE_PATH_PNG_FINAL, ImageFormat.Png);
+            //}
+        }
+
     }
    
 }
