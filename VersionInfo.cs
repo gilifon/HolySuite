9c83189995dac5f1ddc98e4b6c8ab30fbcbf1319
Gil Lianni <gilifon@gmail.com>
2022-07-14 21:43:02 +0300

    Version 5.7.5 - Stable

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -9,5 +9,5 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.7.4")]
-[assembly: AssemblyFileVersion("5.7.4")]
+[assembly: AssemblyVersion("5.7.5")]
+[assembly: AssemblyFileVersion("5.7.5")]
