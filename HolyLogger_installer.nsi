87444c40908938d63662af1fce333441bdc601ab
dan katzman <dankatzman@outlook.com>
2026-05-03 09:43:21 +0300

    v8.1.7: sync splash version and release

--- HolyLogger_installer.nsi
+++ HolyLogger_installer.nsi
@@ -2,7 +2,7 @@
 ; Installs HolyLogger to Program Files and creates Start Menu + Desktop shortcuts
 
 !define APPNAME "HolyLogger"
-!define APPVERSION "8.1.6"
+!define APPVERSION "8.1.7"
 !define MANUFACTURER "4Z1KD"
 !define INSTALL_DIR "$PROGRAMFILES\${MANUFACTURER}\${APPNAME}"
 !define SOURCE_DIR "HolyLogger\bin\x86\Release"
--- VersionInfo.cs
+++ VersionInfo.cs
@@ -9,5 +9,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.1.6")]
-[assembly: AssemblyFileVersion("8.1.6")]
+[assembly: AssemblyVersion("8.1.7")]
+[assembly: AssemblyFileVersion("8.1.7")]
