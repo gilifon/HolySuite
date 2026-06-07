7546609c6c1ffdd9908f027e3d164c2b47175c77
gilifon <gilifon@gmail.com>
2026-06-07 07:44:44 +0300

    Update version to 8.7.1

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -9,6 +9,6 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.7.0")]
-[assembly: AssemblyFileVersion("8.7.0")]
+[assembly: AssemblyVersion("8.7.1")]
+[assembly: AssemblyFileVersion("8.7.1")]
 
